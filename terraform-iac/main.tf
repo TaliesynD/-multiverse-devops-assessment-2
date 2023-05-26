f9dacc3170ada4d6116b69f8b3013eb85202ffdc
TaliesynD <127497844+TaliesynD@users.noreply.github.com>
2023-05-26 06:25:33 +0100

    Remove duplicate S3 bucket definition

--- terraform-iac/main.tf
+++ terraform-iac/main.tf
@@ -1,15 +1,3 @@
 provider "aws" {
 region = "eu-west-2"
 }
-resource "aws_s3_bucket" "this" {
-bucket_prefix = "mvws10-malc"
-force_destroy = true
-tags = {
-Name = "Multiverse WS10 Bucket"
-}
-}
-resource "aws_s3_object" "test_file" {
-bucket = aws_s3_bucket.this.id
-key = "test_file.txt"
-content = "hello world!"
-}
