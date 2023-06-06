5e708d242e45125836562d8d7b509137d8b62bf8
TaliesynD <127497844+TaliesynD@users.noreply.github.com>
2023-06-06 09:15:22 +0100

    Adjust terraform name prefix

--- terraform-iac/security-groups.tf
+++ terraform-iac/security-groups.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "alb" {
-    name_prefix = "mvws9-alb"
+    name_prefix = "mvws10-alb"
     revoke_rules_on_delete = true
     vpc_id = aws_vpc.this.id
     tags = {
@@ -8,7 +8,7 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group" "ec2" {
-    name_prefix = "mvws9-ec2"
+    name_prefix = "mvws10-ec2"
     revoke_rules_on_delete = true
     vpc_id = aws_vpc.this.id
     tags = {
