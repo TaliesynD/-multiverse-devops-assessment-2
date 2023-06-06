2e0604542315ac5c5fe8db70190fe9b192bb0e3d
TaliesynD <127497844+TaliesynD@users.noreply.github.com>
2023-06-06 09:58:55 +0100

    Add ownership controls to S3 bucket

--- terraform-iac/vpc.tf
+++ terraform-iac/vpc.tf
@@ -69,7 +69,17 @@ resource "aws_s3_bucket" "this" {
     }
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
 resource "aws_s3_bucket_acl" "this" {
+    depends_on = [aws_s3_bucket_ownership_controls.this]
+
     bucket = aws_s3_bucket.this.id
     acl = "private"
 }
