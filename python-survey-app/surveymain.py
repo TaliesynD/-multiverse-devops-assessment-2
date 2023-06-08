bb9ac1b8349efdde97b95bcbdcb41e5f9697f2c0
TaliesynD <127497844+TaliesynD@users.noreply.github.com>
2023-06-08 11:05:42 +0100

    more logging

--- python-survey-app/surveymain.py
+++ python-survey-app/surveymain.py
@@ -7,13 +7,13 @@ will be named for the ticket detailing the requirement as per requirements.txt.
 from datetime import datetime
 import os
 
-now = datetime.now()
 cwd = os.getcwd()
 fnamei = cwd  + '/python-survey-app/results.csv'
 fnameo = cwd  + '/python-survey-app/clean_results.csv'
+os.chdir('./python-survey-app/')
+cwd = os.getcwd()
 
-
-print(f"{now}: surveymain started using {fnamei} as input and {fnameo} as output")
+print(f"{datetime.now()}: surveymain started using {fnamei} as input and {fnameo} as output, cwd {cwd}")
 
 from ticket1 import getfile;
 from ticket2 import rm_dups;
@@ -25,35 +25,40 @@ from ticket6 import output_file;
 # call the getfile function to build the array from the input file
 # lines = getfile('c:\Users\MalcBackup\OneDrive\Documents\GitHub\-multiverse-devops-assessment-2\python-survey-app\results.csv')
 # lines = getfile('./results.csv')
+print(f"{datetime.now()}: getfile")
 lines = getfile(fnamei)
 
 
 # print the array line by line
 # print(*lines)
 
+print(f"{datetime.now()}: ignore_empty")
 lines = ignore_empty(lines)
 
 # print the array line by line
 # print(*lines)
 
+print(f"{datetime.now()}: rm_dups")
 lines = rm_dups(lines)
 
 # print the array line by line
 # print(*lines)
 
+print(f"{datetime.now()}: capitalise_names")
 lines = capitalise_names(lines)
 
 # print the array line by line
 # print(*lines)
 
+print(f"{datetime.now()}: rm_out_of_bounds")
 lines = rm_out_of_bounds(lines)
 
 # print the array line by line
 # print(*lines)
 
 # write out the clean results
+print(f"{datetime.now()}: output_file")
 output_file(fnameo,lines)
 
-now = datetime.now()
-print(f"{now}: surveymain completed using {fnamei} as input and {fnameo} as output")
+print(f"{datetime.now()}: surveymain completed using {fnamei} as input and {fnameo} as output")
 
