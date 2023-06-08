480c67eb13f9f188f3d7f74d18c8f1593e3f17e4
TaliesynD <127497844+TaliesynD@users.noreply.github.com>
2023-06-08 11:11:31 +0100

    tree listing

--- python-survey-app/surveymain.py
+++ python-survey-app/surveymain.py
@@ -10,8 +10,12 @@ import os
 cwd = os.getcwd()
 fnamei = cwd  + '/python-survey-app/results.csv'
 fnameo = cwd  + '/python-survey-app/clean_results.csv'
-os.chdir('./python-survey-app/')
-cwd = os.getcwd()
+# os.chdir('./python-survey-app/')
+# cwd = os.getcwd()
+
+for currentpath, folders, files in os.walk('.'):
+    for file in files:
+        print(os.path.join(currentpath, file))
 
 print(f"{datetime.now()}: surveymain started using {fnamei} as input and {fnameo} as output, cwd {cwd}")
 
